3f4b3abb2833f178dfd24b9419198c1b1730ae0a
Adeyemi <adeyemiibrahim990@gmail.com>
2025-06-04 10:49:14 -0700

    Setting up the Project

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,5 @@
-import { Image, Pressable, Text, TouchableOpacity, View } from "react-native";
-import { styles } from "../styles/auth.styles";
+import { Redirect } from 'expo-router'
 
 export default function Index() {
-  return (
-    <View
-      style={styles.container}
-    >
-    </View>
-  );
+  return <Redirect href="/(tabs)" />;
 }
-
---
+++ constants/theme.ts
@@ -0,0 +1,12 @@
+export const COLORS = {
+  primary: "#4ADE80", // Green
+  secondary: "#2DD4BF", // Teal
+  background: "#000000", // Black
+  // textPrimary: "#333333", // Dark Gray
+  surface: "#1A1A1A", // Dark Surface
+  surfaceLight: "#2A2A2A", // Light Surface
+  border: "#E0E0E0", // Light Border
+  white: "#FFFFFF", // Pure White
+  black: "#000000", // Pure Black
+  grey: "#9CA3AF", // Light Gray
+}
