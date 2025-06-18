68570fcf3ee9b4ba1b875e1ed17f5b9c3a3d2968
Adeyemi <adeyemiibrahim990@gmail.com>
2025-06-18 04:47:49 -0700

    Defining Our Schema

---
+++ convex/schema.ts
@@ -0,0 +1,70 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  users: defineTable({
+    // Define the schema for the users table
+    username: v.string(),
+    fullname: v.string(),
+    email: v.string(),
+    bio: v.optional(v.string()),
+    image: v.string(),
+    followers: v.number(),
+    following: v.number(),
+    posts: v.number(),
+    clerkId: v.string(),
+  }).index("by_clerkId", ["clerkId"]),
+
+  posts: defineTable({
+    // Define the schema for the posts table
+    userId: v.id("users"),
+    imageurl: v.string(),
+    storageId: v.id("_storage"), // Reference to the storage table and will be needed to delete a post
+    caption: v.optional(v.string()),
+    likes: v.number(),
+    comments: v.number(),
+  }).index("by_user", ["userId"]),
+
+  likes: defineTable({
+    // Define the schema for the likes table
+    postId: v.id("posts"),
+    userId: v.id("users"),
+  })
+    .index("by_post", ["postId"])
+    .index("by_user_and_post", ["userId", "postId"]),
+
+  comments: defineTable({
+    // Define the schema for the comments table
+    postId: v.id("posts"),
+    userId: v.id("users"),
+    text: v.string(),
+    content: v.string(),
+  }).index("by_post", ["postId"]),
+
+  follows: defineTable({
+    // Define the schema for the follows table
+    followerId: v.id("users"),
+    followingId: v.id("users"),
+  })
+    .index("by_follower", ["followerId"])
+    .index("by_following", ["followingId"])
+    .index("by_both", ["followerId", "followingId"]),
+
+  Notifications: defineTable({
+    // Define the schema for the notifications table
+    receiverId: v.id("users"),
+    senderId: v.id("users"),
+    type: v.union(v.literal("like"), v.literal("comment"), v.literal("follow")),
+    postId: v.optional(v.id("posts")),
+    CommentId: v.optional(v.id("comments")),
+  }).index("by_receiver", ["receiverId"]),
+
+  bookmarks: defineTable({
+    // Define the schema for the bookmarks table
+    userId: v.id("users"),
+    postId: v.id("posts"),
+  })
+    .index("by_user", ["userId"])
+    .index("by_post", ["postId"])
+    .index("by_user_and_post", ["userId", "postId"]),
+});
