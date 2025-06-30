6a09e12347f8fd670b11d1f13e5843e15ff86f77
Adeyemi <adeyemiibrahim990@gmail.com>
2025-06-30 07:53:14 -0700

    Adding Bookmarks

--- components/Post.tsx
+++ components/Post.tsx
@@ -31,11 +31,13 @@ type postProps = {
 
 export default function Post({post}:  postProps ) {
   const [  isLiked, setIsLiked] = useState(post.isLiked);
+  const [  isBookmarked, setIsBookmarked] = useState(post.isBookmarked);
   const [ likesCount, setLikesCount] = useState(post.likes);
   const [ commentsCount, setCommentsCount] = useState(post.comments);
   const [showComments, setShowComments] = useState(false);
 
   const toggleLike = useMutation(api.posts.toggleLike);
+  const toggleBookmark = useMutation(api.bookmarks.toggleBookmark);
 
   const handlelike = async () => {
     try {
@@ -45,6 +47,11 @@ export default function Post({post}:  postProps ) {
     } catch (error) {
       console.error("Error toggling like:", error);
     }
+  }
+
+  const handleBookmark = async () => {
+    const newIsBookmarked = await toggleBookmark({ postId: post._id });
+    setIsBookmarked(newIsBookmarked);
   }
 
   return (
@@ -97,8 +104,8 @@ export default function Post({post}:  postProps ) {
           <Ionicons name="chatbubble-outline" size={24} color={COLORS.white} />
         </TouchableOpacity>
         </View>
-        <TouchableOpacity>
-          <Ionicons name="bookmark-outline" size={22} color={COLORS.white} />
+        <TouchableOpacity onPress={handleBookmark}>
+          <Ionicons name={isBookmarked? "bookmark" : "bookmark-outline"} size={22} color={COLORS.white} />
         </TouchableOpacity>
       </View>
 
