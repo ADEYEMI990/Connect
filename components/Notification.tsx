626c382e14267c769abbf48d490eaf596b299f91
Adeyemi <adeyemiibrahim990@gmail.com>
2025-07-07 08:39:26 -0700

    Dynamic User Screen

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -11,7 +11,7 @@ export function Notification({ notification }: any) {
   return (
     <View style={styles.notificationItem}>
       <View style={styles.notificationContent}>
-        <Link href={`/notifications`} asChild>
+        <Link href={`/user/${notification.sender._id}`} asChild>
           <TouchableOpacity style={styles.avatarContainer}>
             <Image 
               source={notification.sender.image }
